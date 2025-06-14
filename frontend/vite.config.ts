d1362014d2516fc6f400aeb593a3547d98432523
Dario <dariojakovleski10@gmail.com>
2025-06-14 13:25:28 +0200

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss()],
     server: {
         port: 3333,
         proxy: {
-        '/api': {
-            target: 'http://localhost:8080',
-            changeOrigin: true,
-            secure: false,
-        },
+            '/api': {
+                target: 'http://sports.local:8088',
+                changeOrigin: true,
+                secure: false,
+            },
         },
     },
 })
